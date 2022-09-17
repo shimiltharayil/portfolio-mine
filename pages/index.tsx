7ba30f43c34b48cb635cf6dfa02b3cfc75aec5ec
Shimil Tharayil <shimiltharayil007@gmail.com>
2022-09-17 16:03:03 +0530

    Intial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Shimil Tharayil</title>
+       
+      </Head>
+       <h1 className='text-red-500' >Hello world</h1>
+     
+    </div>
+  )
+}
+
+export default Home
